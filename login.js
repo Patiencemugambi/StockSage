d268cb1bbcc04bfc879a5767cc49f0202a920346
Patiencemugambi <patiencemugambi@gmail.com>
2023-07-07 20:44:45 +0300

    login page authentication

--- login.js
+++ login.js
@@ -1,3 +1,19 @@
+// const loginForm = document.getElementById('login-form');
+
+// loginForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   const email = document.getElementById('email').value;
+//   const password = document.getElementById('password').value;
+
+//   // Perform authentication or login logic here
+//   // You can send the form data to a server for verification
+
+//   loginForm.reset();
+//   window.location.href = 'index.html'; // Redirect to homepage.html after successful login
+// });
+
+
 const loginForm = document.getElementById('login-form');
 
 loginForm.addEventListener('submit', function(event) {
@@ -6,9 +22,25 @@ loginForm.addEventListener('submit', function(event) {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  // Perform authentication or login logic here
-  // You can send the form data to a server for verification
+  fetch('./db.json')
+    .then(response => response.json())
+    .then(data => {
+      // Find user with matching email and password
+      const user = data.users.find(user => user.email === email && user.password === password);
+
+      if (user) {
+        // Authentication successful
+        alert('Login successful!');
+        loginForm.reset();
+        window.location.href = 'index.html'; // Redirect to homepage.html after successful login
+      } else {
+        // Authentication failed
+        alert('Invalid email or password. Please try again.');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 
   loginForm.reset();
-  window.location.href = 'index.html'; // Redirect to homepage.html after successful login
 });
